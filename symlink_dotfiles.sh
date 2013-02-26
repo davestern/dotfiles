296ef12e1fd32e4276fb73626dfa114ff55f97c9
Dave Stern <dave@davestern.com>
2013-02-26 00:22:52 -0500

    Finished up symlink, backup functionality

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 ############################
-##
+## symlink_dotfiles.sh
 ############################
 
-
+## Symlinks each dotfiles/file that is not .md, .sh or ~
+## to the corresponding ~/.file name and makes a backup
+## if a regular file already exists.
 
 DOTFILES_DIRECTORY=$HOME/dotfiles
 
@@ -13,8 +15,24 @@ DOTFILES_DIRECTORY=$HOME/dotfiles
 ## back it up with a dated version if it's not already a symlink to 
 ## the dotfiles version and make the symlink
 
-for f in `ls $DOTFILES_DIRECTORY`; do
-echo $f;
+FILES=$(ls $DOTFILES_DIRECTORY | grep -v -P '(md|sh|~)$')
+
+for file in $FILES; do
+    dotfile=~/.${file}
+    ## Only continue if the dotfile is not already a symbolic link
+    if [ ! -L $dotfile ]
+    then
+
+	## If the dotfile is a regular file, back it up
+	if [ -f $dotfile ]
+	then
+	    backup=$dotfile.$(date +%Y%m%d%H%M%S)
+	    echo "Backing up regular file $dotfile to $backup"
+	    mv $dotfile $backup
+	fi;
+
+	ln -v -s ~/dotfiles/${file} $dotfile
+    fi;
 done;   
 
 
